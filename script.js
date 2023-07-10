d1c27150d4a5215be6df1a7f803de40aaeb5713a
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-10 13:01:35 -0600

    moved the query selector to hide guessing number

--- script.js
+++ script.js
@@ -14,8 +14,6 @@ console.log(document.querySelector('.guess').value);
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
-
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
@@ -28,6 +26,7 @@ document.querySelector('.check').addEventListener('click', function () {
     // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🏆 Correct Number!';
+    document.querySelector('.number').textContent = secretNumber;
 
     // This controls the CSS background color when  you win to green
     document.querySelector('body').style.backgroundColor = '#60b347';
