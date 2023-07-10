977c605d87a892016ab0620e7a108fc39bf72bba
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-10 11:09:25 -0600

    adding click and making btn work and deliver a message

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 'use strict';
-'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🏆Correct Number!';
 console.log(document.querySelector('.message').textContent);
@@ -10,3 +9,17 @@ document.querySelector('score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫No Number!';
+  }
+});
